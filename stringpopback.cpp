ab6c1ac0365702d74ac8d8f9bd37a8742d84f543
Sajib Ahmed <112968850+mollah2022@users.noreply.github.com>
2023-07-20 22:09:40 +0600

    Add files via upload

---
+++ stringpopback.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int32_t main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	string s1;
+	getline(cin,s1);
+	cout << s1 << endl;
+	s1.pop_back();
+		cout << s1 << endl;
+	
+      return 0;
+}
