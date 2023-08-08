ac5aa5137e99ef5a93e7f574ef4ea7222b9243bb
Sajib Ahmed <112968850+mollah2022@users.noreply.github.com>
2023-08-09 00:27:03 +0600

    Add files via upload

---
+++ and_operation.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int32_t main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	int x,y; cin >> x >> y;
+	int ans = 0;
+
+	ans = (x&y);
+	cout << ans << endl;
+
+	return 0;
+}
---
+++ left_shift.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int32_t main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	int x; cin >> x;
+	int ans = 0;
+
+	ans = x << 3;
+	cout << ans << endl;
+	return 0;
+}
