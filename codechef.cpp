d71156427b041f1445cc42c3f4f06cc6d840f6b1
Sajib Ahmed <112968850+mollah2022@users.noreply.github.com>
2024-10-11 19:33:31 +0600

    Add files via upload

--- codechef.cpp
+++ codechef.cpp
@@ -1,39 +1,35 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
 using namespace std;
 
-int main() {
-	// your code goes here
-	int t; cin >> t;
-	while(t--){
-	       int n; cin >> n;
-	       vector<int>v1;
-	       for(int i=0;i<n;i++){
-	             int y; cin >> y;
-	             v1.push_back(y);
-	       }
-	       sort(v1.begin(),v1.end());
-	       reverse(v1.begin(),v1.end());
-	       int l = v1.size();
-	       int sum1=0,sum2=0;
-	       if(l%2!=0){
-                       for(int i=0;i<=v1.size()/2;i++){
-	             sum1+=v1[i];
-	       }
-	        for(int i=v1.size()/2+1;i<v1.size();i++){
-	             sum2+=v1[i];
-	       }
-               cout << sum1-sum2 << endl;
-               continue;
-	       }
-	       
-	       for(int i=0;i<v1.size()/2;i++){
-	             sum1+=v1[i];
-	       }
-	        for(int i=v1.size()/2;i<v1.size();i++){
-	             sum2+=v1[i];
-	       }
+void constructMatrix(int N, int M) {
+    vector<vector<int>> matrix(N, vector<int>(M));
+    int base = 2;  
 
-	       cout << sum1-sum2 << endl;
-	}
-  return 0;
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < M; ++j) {
+
+            matrix[i][j] = base + i + j;
+        }
+    }
+
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < M; ++j) {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    int T;
+    cin >> T;  
+
+    while (T--) {
+        int N, M;
+        cin >> N >> M;  
+        constructMatrix(N, M);
+    }
+
+    return 0;
 }
