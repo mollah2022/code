d628690fe6b92d8749910a30541b5edb59a0d118
Sajib Ahmed <112968850+mollah2022@users.noreply.github.com>
2024-10-17 21:52:49 +0600

    Add files via upload

--- codechef.cpp
+++ codechef.cpp
@@ -1,35 +1,31 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
-
-void constructMatrix(int N, int M) {
-    vector<vector<int>> matrix(N, vector<int>(M));
-    int base = 2;  
-
-    for (int i = 0; i < N; ++i) {
-        for (int j = 0; j < M; ++j) {
-
-            matrix[i][j] = base + i + j;
-        }
-    }
-
-    for (int i = 0; i < N; ++i) {
-        for (int j = 0; j < M; ++j) {
-            cout << matrix[i][j] << " ";
-        }
-        cout << endl;
-    }
-}
 
 int main() {
     int T;
     cin >> T;  
-
+    
     while (T--) {
-        int N, M;
-        cin >> N >> M;  
-        constructMatrix(N, M);
+        int N, K;
+        cin >> N >> K;  
+        
+        vector<int> A(N);
+        for (int i = 0; i < N; i++) {
+            cin >> A[i];  
+        }
+        
+        sort(A.begin(), A.end());
+        
+        if( K==1 ){
+            int value = (2*(A[N-1]))+A[0]+A[N-2];
+            cout << value << endl;
+            continue;
+        }
+          int score = A[N-1-K]+A[N-2]+A[0]+A[N-1];
+        cout << score << endl;
     }
-
+    
     return 0;
 }
